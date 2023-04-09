6a4ac3b50ebe0d65491797722ef1e9bed2bded27
EmanoelG <emanoelgalvao42@gmail.com>
2023-04-09 20:10:05 -0300

    add teste unitari
    teste unitario

---
+++ src/test/java/br/com/emanoel/habilidades/services/PersonServiceTest.java
@@ -0,0 +1,44 @@
+package br.com.emanoel.habilidades.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonServiceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateV2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
