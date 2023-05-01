20bba08b7448e3814d6a2ef4e530714b4740e34e
EmanoelG <emanoelgalvao42@gmail.com>
2023-05-01 17:21:34 -0300

    swagger doc endpoints
    swagger doc endpoints ok

--- src/main/java/br/com/emanoel/habilidades/controllers/PersonController.java
+++ src/main/java/br/com/emanoel/habilidades/controllers/PersonController.java
@@ -18,29 +18,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.emanoel.habilidades.data.vo.v1.PersonVO;
 import br.com.emanoel.habilidades.data.vo.v2.PersonVOV2;
+import br.com.emanoel.habilidades.models.Person;
 import br.com.emanoel.habilidades.services.PersonServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/api/persons/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController {
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	@Autowired
 	private PersonServices service;
 
 	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@Operation(summary = "Busca pessoa por ID ", description = "Busca pessoa por ID", tags = { "People" }, responses = {
+			@ApiResponse(responseCode = "200", content = {
+			@ApiResponse(responseCode = "400", content = @Content, description = "Bad request"),
+			@ApiResponse(responseCode = "401", content = @Content, description = "Unathorized"),
+			@ApiResponse(responseCode = "500", content = @Content, description = "Internal Error")
+
+	}
+
+	)
 	public PersonVO findById(@PathVariable("id") Long id) throws Exception {
 		return service.findById(id);
 	}
-	
+
 	@PutMapping(value = "/", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public PersonVO update(@RequestBody PersonVO person) throws Exception {
 		logger.info("Upadte user  !!!");
 		return service.update(person);
 	}
-	
-	@GetMapping(produces ={ MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@Operation(summary = "Retorna todas as pessoas ", description = "Retorna todas as pessoas", tags = {
+			"People" }, responses = { @ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PersonVO.class, allOf = Person.class))) }, description = "Sucess"),
+					@ApiResponse(responseCode = "400", content = @Content, description = "Bad request"),
+					@ApiResponse(responseCode = "401", content = @Content, description = "Unathorized"),
+					@ApiResponse(responseCode = "500", content = @Content, description = "Internal Error")
+
+	}
+
+	)
+
 	public List<PersonVO> findAll() throws Exception {
-		
+
 		return service.findAll();
 	}
 
